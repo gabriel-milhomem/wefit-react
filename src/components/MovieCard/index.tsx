0ab32974e396da054e497b25948e4c6d786a31ac
Gabriel Milhomem Cunha <gabriell.mil@gmail.com>
2022-12-13 21:03:36 -0300

    feat: static MovieCard component

---
+++ src/components/MovieCard/index.tsx
@@ -0,0 +1,31 @@
+import {
+  MovieQuantity,
+  Card,
+  MovieImage,
+  MovieName,
+  MoviePrice,
+} from './styles'
+import { Button } from '../Button'
+
+import { ReactComponent as ShoppingCartIcon } from '../../assets/shopping-cart.svg'
+
+export function MovieCard() {
+  return (
+    <Card>
+      <MovieImage>
+        <img
+          src="https://www.imagemhost.com.br/images/2022/07/10/viuva-negra.png"
+          alt="DVD da Viuva negra"
+        />
+      </MovieImage>
+
+      <MovieName>Viúva Negra</MovieName>
+      <MoviePrice>R$ 9.99</MoviePrice>
+      <Button>
+        <ShoppingCartIcon />
+        <MovieQuantity> 0 </MovieQuantity>
+        <p> ADICIONAR AO CARRINHO</p>
+      </Button>
+    </Card>
+  )
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
 export * from './Header'
+export * from './MovieCard'
+export * from './Alert'
